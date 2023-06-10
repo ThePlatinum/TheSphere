641cf0eabbf1775682acca91c818a85a83d80ffd
Platinum <platinumemirate@gmail.com>
2023-06-10 04:47:07 +0100

    handle admin role

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+use Spatie\Permission\Middlewares\RoleMiddleware as MiddlewaresRoleMiddleware;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware extends MiddlewaresRoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next, $role, $guard = null)
+    {
+        $authGuard = Auth::guard($guard);
+
+        if ($authGuard->guest()) {
+            throw UnauthorizedException::notLoggedIn();
+        }
+
+        $roles = is_array($role)
+            ? $role
+            : explode('|', $role);
+
+        if (! $authGuard->user()->hasAnyRole($roles)) {
+            // throw UnauthorizedException::forRoles($roles);
+
+            return redirect()->away(config('app.frontend_url'));
+        }
+
+        return $next($request);
+    }
+}
---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+
+        // Create Roles
+        Role::create(['name' => 'admin']);
+    }
+}
